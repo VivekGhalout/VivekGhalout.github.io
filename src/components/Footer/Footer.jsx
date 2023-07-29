34b0762090665e5d099cbfa72fe9e09f35258dbc
VivekGhalout <vivek.ghalout@gmail.com>
2023-07-29 12:50:21 +0530

    margin adjestment

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,9 +14,6 @@ function Footer() {
         <div className="wave" id="wave4"></div>
       </div>
       <ul className="social-icon">
-        {/* <li className="social-icon__item"><a className="social-icon__link" href="#">
-          <FaTwitter/>
-        </a></li> */}
         <li className="social-icon__item"><a className="social-icon__link" href="https://www.linkedin.com/in/vivek-kumar-ghalout-a655551a1/" target='_blank'>
           <BsLinkedin/>
         </a></li>
@@ -24,14 +21,6 @@ function Footer() {
           <FaGithub/>
         </a></li>
       </ul>
-      {/* <ul className="menu">
-        <li className="menu__item"><a className="menu__link" href="#">Home</a></li>
-        <li className="menu__item"><a className="menu__link" href="#">About</a></li>
-        <li className="menu__item"><a className="menu__link" href="#">Skills</a></li>
-        <li className="menu__item"><a className="menu__link" href="#">Projects</a></li>
-        <li className="menu__item"><a className="menu__link" href="#">Contact</a></li>
-
-      </ul> */}
       <p>&copy;2023 Vivek Kumar Ghalout | All Rights Reserved</p>
     </footer>
   )
